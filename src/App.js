fa4cca6ee05b9b14b36b0dbf928708c06a7c6a37
Manuel <53manuelrodriguez@gmail.com>
2022-11-16 12:49:55 -0500

    prueba

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Fragment, useState, useEffect} from 'react';
+import Navbar from './Components/Navbar';
+import BookList from './Components/BookList';
+
 
 function App() {
+  
+  
+  const [people, setPeople] = useState([])
+
+  useEffect(()=>{
+   const getpeople = () => {
+    fetch('https://swapi.dev/api/people/')
+    .then(res => res.json())
+    .then(res => console.log(res))
+   }
+   getpeople()
+  },[])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Navbar brand="Library App"/>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-7'>
+            <h2 style={{textAlign: "center"}}>Lista de personajes</h2>
+              <BookList people={people}/>
+          </div>
+          <div className='col-5'>
+          <h2 style={{textAlign: "center"}}>Lista de personajes</h2>
+          </div>
+        </div>
+      </div>
+    </Fragment>
   );
 }
 
---
+++ src/Components/BookList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const BookList = () => {
+    return ( 
+        <table className='table'>
+            <thead>
+                <tr>
+                <th>Nombre</th>
+                <th>Peso</th>
+                <th>Altura</th>
+                </tr>
+                
+            </thead>
+            
+        </table>
+     );
+}
+ 
+export default BookList;
